62db804e90d8f2343758ca1e49b17fec4b244fe0
Hamza <199155278+hsale007@users.noreply.github.com>
2025-05-10 20:05:36 -0400

    Converted from factory functions to classes

--- script.js
+++ script.js
@@ -1,17 +1,60 @@
 const table = document.querySelector("table");
 const tableBody = document.querySelector("tbody");
 
-function Library() {
-  this.books = [];
+class Library {
+  constructor() {
+    this.books = [];
+  }
+
+  addToTable(book) {
+    const row = document.createElement("tr");
+    console.log(book);
+    for (const key of Object.keys(book1)) {
+      const cell = document.createElement("td");
+      cell.innerText =
+        key === "read" ? (book[key] ? "Read" : "Not Read") : book[key];
+      row.appendChild(cell);
+      if (key === "read") row.appendChild(addUserOptions(book["read"]));
+    }
+    //add user options to each row in table
+    tableBody.append(row);
+  }
+
+  addBooktoLibrary(book) {
+    this.books.push(book);
+    this.addToTable(book);
+  }
+
+  clearLibrary() {
+    this.books = [];
+  }
 }
 
-function Book(title, author, pages, read) {
-  this.id = crypto.randomUUID();
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(title, author, pages, read) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
+
+const library = new Library();
+
+const book1 = new Book("Fahrenheit 451", "Ray Bradbury", "249", true);
+const book2 = new Book("1984", "Geroge Orwell", "328", true);
+const book3 = new Book("To Kill a Mockingbird", "Harper Lee", "336", false);
+const book4 = new Book("Lord of the FLies", "William Golding", "224", false);
+for (const book of [book1, book2, book3, book4]) library.addBooktoLibrary(book);
+
+//create table for books
+
+const button = document.querySelector(".add-book");
+const dialogElement = document.querySelector("#new-book");
+const clearTable = document.querySelector(".clear-table");
+const form = document.querySelector("form");
+const cancelNewBook = document.querySelector(".cancel-new-book");
 
 function addUserOptions(isRead) {
   const cell = document.createElement("td");
@@ -66,47 +109,6 @@ function toggleReadStatus(isRead) {
   return check;
 }
 
-const library = new Library();
-
-Library.prototype.addToTable = function (book) {
-  const row = document.createElement("tr");
-  for (const key of Object.keys(book1)) {
-    const cell = document.createElement("td");
-    cell.innerText =
-      key === "read" ? (book[key] ? "Read" : "Not Read") : book[key];
-    row.appendChild(cell);
-    if (key === "read") row.appendChild(addUserOptions(book["read"]));
-  }
-  //add user options to each row in table
-  tableBody.append(row);
-};
-
-Library.prototype.addBooktoLibrary = function () {
-  library.books.push(this);
-  library.addToTable(this);
-};
-
-Book.prototype.createBook = function (title, author, pages, read) {
-  const book = new Book(title, author, pages, read);
-};
-
-Object.setPrototypeOf(Book.prototype, Library.prototype);
-
-const book1 = new Book("Fahrenheit 451", "Ray Bradbury", "249", true);
-const book2 = new Book("1984", "Geroge Orwell", "328", true);
-const book3 = new Book("To Kill a Mockingbird", "Harper Lee", "336", false);
-const book4 = new Book("Lord of the FLies", "William Golding", "224", false);
-const bookList = [book1, book2, book3, book4];
-for (const book of bookList) book.addBooktoLibrary();
-
-//create table for books
-
-const button = document.querySelector(".add-book");
-const dialogElement = document.querySelector("#new-book");
-const clearTable = document.querySelector(".clear-table");
-const form = document.querySelector("form");
-const cancelNewBook = document.querySelector(".cancel-new-book");
-
 button.addEventListener("click", () => {
   dialogElement.showModal();
 });
@@ -127,6 +129,7 @@ clearTable.addEventListener("click", () => {
     table.removeChild(tableBody);
     const newTableBody = document.createElement("tbody");
     table.appendChild(newTableBody);
+    library.clearLibrary();
   }
 });
 
@@ -140,7 +143,7 @@ form.addEventListener("submit", (e) => {
     userValues.pages.value,
     userValues.read.checked
   );
-  book.addBooktoLibrary();
+  library.addBooktoLibrary(book);
   console.log(book);
   dialogElement.close();
   form.reset();
